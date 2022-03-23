5d15f3dd38cddce9bab371a8a5cd723129c5f10b
Matheus Martins Susin <matheus.susin@venturus.org.br>
2022-03-23 12:46:07 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Matheus Martins Susin on 2022-03-23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
